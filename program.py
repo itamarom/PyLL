a4aac0a93c59aa96e7156ac7b88abcf2d13a610d
itkeman <itamarnine4@gmail.com>
2015-07-03 21:31:00 +0300

    Fixed typo. Started writing code to parse opcodes. This is still a WIP

--- program.py
+++ program.py
@@ -10,14 +10,23 @@ class Program(object):
         self.current_inst = None
         
     def run(self, entry_point='main', args=None):
-        self.current_inst = (self.funs[entry_point], 0, {})
+        self.current_inst = (self.funcs[entry_point], 0, {})
         
         while True:
             self._exec_inst()
             
             
     def _exec_inst(self):
-        raise NotImplementedError()
+        RESULT_OPCODE_PATTERN = r"%\w+\s*=\s*"
+        func, op_index, scope = self.current_inst
+        op_text = func['content'][op_index]
+        
+        if re.findall(RESULT_OPCODE_PATTERN, op_text) and
+           op_text.find(re.findall(RESULT_OPCODE_PATTERN, op_text)[0]) == 0:
+           
+           dest, opcode = op_text.split('=', 1)
+        
       
+        self.current_inst = func, op_index+1, scope
     
         
