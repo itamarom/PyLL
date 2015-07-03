0d8f12f7e4a4fb16f8c86d724d79ac6a441d9e1f
itkeman <itamarnine4@gmail.com>
2015-07-03 21:04:38 +0300

    Renamed variable and added scope to current inst

--- program.py
+++ program.py
@@ -7,10 +7,10 @@ class Program(object):
         self.attribs = attribs
         
         self.callstack = []
-        self.current_instruction = None
+        self.current_inst = None
         
     def run(self, entry_point='main', args=None):
-        self.current_instruction = (self.functions[entry_point], 0)
+        self.current_inst = (self.funs[entry_point], 0, {})
         
         while True:
             self._exec_inst()
