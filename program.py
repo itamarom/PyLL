713c94d1244770309326621f8b3a18742af42c22
Yuval Gitlitz <gitlitzo@gmail.com>
2015-07-03 21:20:20 +0300

    added stubs for Opcodes

--- program.py
+++ program.py
@@ -1,23 +1,31 @@
 class Program(object):
-    
     def __init__(self, unknown, globs, funcs, attribs):
         self.unknown = unknown
         self.globs = globs
         self.funcs = funcs
         self.attribs = attribs
-        
+
         self.callstack = []
         self.current_inst = None
-        
+
     def run(self, entry_point='main', args=None):
         self.current_inst = (self.funs[entry_point], 0, {})
-        
+
         while True:
             self._exec_inst()
-            
-            
+
     def _exec_inst(self):
         raise NotImplementedError()
-      
-    
-        
+
+class Opcodes(object):
+    def alloca(self, *args):
+        # implemented by Itamar
+        pass
+
+    def store(self, *args):
+        # Implemented by Amit
+        pass
+
+    def call(self, *args):
+        # Implemented by Gitlitz
+        pass
