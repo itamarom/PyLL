ce048ed1077fcbc6a4b6250d4ab3a5932aceb034
itkeman <itamarnine4@gmail.com>
2015-07-10 18:59:31 +0300

    redid types

--- lltypes.py
+++ lltypes.py
@@ -1,47 +1,52 @@
-class LLType(object):
-    name = None
-   
-class LLPointerType(LLType):
-    def __init__(self, obj):
-        self.name = obj.name + "*"
-        self.obj = obj
 
-    @classmethod
-    def intialize(cls, obj):
-        return LLPointerType(obj)
-    
-class LLBufferType(LLType):
-    size = None
+class LLVar(object):
+
+    @property
+    def value(self):
+        print "a"
+        return self._value
         
-    @classmethod
-    def create(cls):
-        return bytearray(cls.size)
+    @value.setter
+    def value(self, new_value):
+        print "b"
+        self._validate_type(new_value)
+        self._value = new_value
+     
+    def _validate_type(self, new_value):
+        raise NotImplementedError()
+
+class LLPointer(LLVar):
+    def __init__(self, value):
+        self.value = value
         
-class I32(LLBufferType):
-    name = 'i32'
-    size = 4
-    
-    @classmethod
-    def intialize(cls, value):
-        obj = cls.create()
-        struct.pack_into('I', obj, 0, int(value))
-        return obj
+    def _validate_type(self, value):
+        assert(value is None or type(value).__name__ == self.obj_type.__name__)
+
+class LLInt(LLVar):
+    def __init__(self, value):
+        value = int(value)
+        self.value = value
         
-class I8(LLBufferType):
-    name = 'i8'
-    size = 1
+    def _validate_type(self, value):
+        assert(type(self).MIN <= value <= type(self).MAX)
+        
+class I32(LLInt):
+    MIN = 0
+    MAX = 2 ** 32
     
-    @classmethod
-    def intialize(cls, value):
-        obj = cls.create()
-        struct.pack_into('b', obj, 0, int(value))
-        return obj
+class I8(LLInt):
+    MIN = 0
+    MAX = 2 ** 8
 
 def get_type(name):
     if name in TYPES:
         return TYPES[name]
-    
+        
+    if name.endswith('*'):
+        return type(name.replace("*", "Pointer"), (LLPointer, ), { 'obj_type': get_type(name[:-1])})
+
     raise LLTypeNotFound("Could not find type: '%s'" % name)
         
 TYPES = {}
 TYPES['i32'] = I32
+TYPES['i8'] = I8
